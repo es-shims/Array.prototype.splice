2d9750b82747886650987d1e4dd5c3a846984018
Jordan Harband <ljharb@gmail.com>
2022-04-13 22:35:19 -0700

    [Tests] skip strict mode tests in non-strict-mode engines

--- test/implementation.js
+++ test/implementation.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var implementation = require('../implementation');
 var callBind = require('call-bind');
+var hasStrictMode = require('has-strict-mode')();
 var test = require('tape');
+
+var implementation = require('../implementation');
 var runTests = require('./tests');
 
 test('as a function', function (t) {
-	t.test('bad array/this value', function (st) {
+	t.test('bad array/this value', { skip: !hasStrictMode }, function (st) {
 		st['throws'](callBind(implementation, null, undefined, 'a'), TypeError, 'undefined is not an object');
 		st['throws'](callBind(implementation, null, null, 'a'), TypeError, 'null is not an object');
 		st.end();
